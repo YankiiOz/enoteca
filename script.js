03935b612c2457b601811ddfc48ab21a312dcaab
Яна <68820959+YankiiOz@users.noreply.github.com>
2021-02-10 18:56:22 +0300

    Add files via upload
    added a script with objects

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+let firstWine = {
+    name: 'Вино grin Pinot Grigio, 2018г.',
+    Страна: 'Италия',
+    Цвет: 'Белое',
+    Сахар: 'Сухое',
+    Виноград: 'Пино Гриджо',
+    Производитель: 'Volpe Pasini',
+    image: enoteca.images/grin-pino-grigio.webp
+}
+
+let secondWine = {
+    name: 'Вино Chianti Castiglioni, 2019г.',
+    Страна: 'Италия',
+    Цвет: 'Красное',
+    Сахар: 'Сухое',
+    Виноград: 'Санджовезе',
+    Производитель: 'Frescobaldi',
+    image: enoteca.images/wine.webp
+}
+
+let thirdWine = {
+    name: 'Вино Bin 65 Chardonnay, 2019г.',
+    Страна: 'Австралия',
+    Цвет: 'Белое',
+    Сахар: 'Полусухое',
+    Виноград: 'Шардоне',
+    Производитель: 'Lindemans',
+    image: enoteca.images/lindemans.webp
+}
+
+let fourthWine = {
+    name: 'Вино Valpolicella Classico, 2019 г.',
+    Страна: 'Италия',
+    Цвет: 'Красное',
+    Сахар: 'Полусухое',
+    Виноград: 'Корвиноне',
+    Производитель: 'Domini Veneti',
+    image: enoteca.images/valpolicella.webp
+}
+
+let fifthWine = {
+    name: 'Вино Tavel, Guigal, 2019 г.',
+    Страна: 'Франция',
+    Цвет: 'Розовое',
+    Сахар: 'Сухое',
+    Виноград: 'Гренаш',
+    Производитель: 'Guigal',
+    image: enoteca.images/tavel.webp
+
+}
+
+let sixthWine = {
+    name: 'Вино Romio Pinot Grigio, 2019 г.',
+    Страна: 'Италия',
+    Цвет: 'Белое',
+    Сахар: 'Полусухое',
+    Виноград: 'Пино Гриджо',
+    Производитель: 'Caviro',
+    image: enoteca.images/romino.webp
+}
+
+
+let wine = [firstWine, secondWine, thirdWine, fourthWine, fifthWine, sixthWine];
